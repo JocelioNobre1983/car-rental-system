14cc472f5f397427bbd456e180d5fa0d19a996ad
JocelioNobre1983 <JocelioNobre1983>
2025-10-24 09:51:14 -0300

    "Created class Brazil Tax Service"

---
+++ src/model/services/BrazilTaxService.java
@@ -0,0 +1,12 @@
+package model.services;
+
+//CLASS RESPONSIBLE FOR CALCULATING THE INVOICE TAX
+public class BrazilTaxService {
+    private Double tax(Double amount){
+        if(amount <= 100.00){
+            return amount * 0.20;
+        } else {
+            return amount * 0.15;
+        }
+    }
+}
